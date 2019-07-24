0903867cd920c564e297ef9649faf258452fb24c
ajgeiss0702 <ajgeiss72@gmail.com>
2019-07-23 18:51:24 -0700

    stuff

--- js/pageloader.js
+++ js/pageloader.js
@@ -1,22 +1,36 @@
-var activePage = "schoolselector"
+var activePage = "dummy";
 
 function loadContent(pageName) {
+  $('#load').slideDown('fast');
+  $('#'+activePage).slideUp()
+  $('#'+activePage).removeClass('fadeIn');
+  $('#'+activePage).addClass('fadeOut');
   httpGet('/pages/'+pageName+'.html').then((c) => {
     var elem = $('#'+pageName);
-    $('#'+activePage).slideUp()
-    $('#'+activePage).removeClass('fadein');
-    $('#'+activePage).addClass('fadeOut');
+    elem.html(c);
     activePage = pageName;
     elem.slideDown();
-    elem[0].classList = 'fadein'
-    elem.html(c);
+    elem[0].classList = 'fadeIn'
     if(pageName == 'countdown') {
       insertNews();
     }
+    location.hash = "#"+pageName;
+    $('#load').slideUp('fast');
   })
 }
 
+$(window).on('hashchange', function() {
+  var newhash = location.hash.split('#').splice(1).join('#');
+  if(location.hash != "" && newhash != activePage) {
+    loadContent(newhash);
+  }
+});
+
 async function pageInit() {
+  if(location.hash != "") {
+    loadContent(location.hash.split('#').splice(1).join('#'));
+    return;
+  }
   if(typeof rcf.school == 'undefined') {
     loadContent('schoolselector');
   } else if(!(await schoolExists(rcf.school))) {
@@ -25,4 +39,6 @@ async function pageInit() {
     loadContent('countdown');
   }
 }
-pageInit();
+rcf.on('load', () => {
+  pageInit();
+})

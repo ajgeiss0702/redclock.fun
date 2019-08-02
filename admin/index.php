51dff18bd984b246768b6b51d7e530b1e78eb241
ajgeiss0702 <ajgeiss72@gmail.com>
2019-08-01 21:25:02 -0700

    more stuff on admin page

--- admin/index.php
+++ admin/index.php
@@ -1,1 +1,10 @@
-hi
+<html>
+  <head>
+    <script src="https://cdn.jsdelivr.net/npm/chart.js@2.8.0" defer></script>
+  </head>
+  <body>
+    <div style='text-align: center;margin-left:auto;margin-right:auto;'>
+      
+    </div>
+  </body>
+</html>

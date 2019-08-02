94895b187e2431d4250ee2f4348e8fe5555181b3
ajgeiss0702 <ajgeiss72@gmail.com>
2019-08-02 09:20:05 -0700

    add back bonk page

---
+++ bonk.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Bonk.io - Just bonk!</title>
+		<meta name="Description" content="Bonk without ads. Only bonk. Works with game links (just change .io to .fun in url). ">
+		<link rel="icon" type="image/png" href="https://bonk.io/tt/favicon-32x32.png">
+		<style>
+		body, html {
+			margin: 0;
+			min-height: 100%;
+			overflow-y: hidden;
+		}
+		</style>
+		<!-- Global site tag (gtag.js) - Google Analytics -->
+		<script async src="https://www.googletagmanager.com/gtag/js?id=UA-106568388-4"></script>
+		<script>
+		  window.dataLayer = window.dataLayer || [];
+		  function gtag(){dataLayer.push(arguments);}
+		  gtag('js', new Date());
+
+		  gtag('config', 'UA-106568388-4');
+		</script>
+	</head>
+	<body>
+		<object type="application/x-shockwave-flash" data="https://bonk.io/BonkLiveVersion2.swf" style="width: 100%; height: 100vh;">
+				<param name="movie" value="https://bonk.io/BonkLiveVersion2.swf">
+				<param name="FlashVars" value="autoconnectroomid=<?php
+				if(isset($_GET['r'])) {
+					print($_GET['r']);
+				}
+				?>&autoconnectroompassword=">
+				<param name="wmode" value="direct">
+				<param name="allowscriptaccess" value="always">
+
+		</object>
+	</body>
+</html>

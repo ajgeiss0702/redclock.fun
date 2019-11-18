4cd5df81b24ad880d5920284e6b1b701b54d1329
ajgeiss0702 <ajgeiss72@gmail.com>
2019-11-18 13:54:48 -0700

    will automatically get rid of festiveness after holidays

--- js/festive.js
+++ js/festive.js
@@ -2,12 +2,15 @@ rcf.on('load', festive);
 settings.create("festive", true, "Festive", "Holiday Festiveness");
 function festive() {
   if(typeof _GET("rmhs") != "string") {
-    if(settings.get("festive")) {
-      $('#festive').html(`
-        <img src="/img/lights.gif" class='festive-image'>
-        `);
-    } else {
-      $('#festive').html(``);
+    var month = new Date().getMonth();
+    if((month == 10 || month == 11)) {
+      if(settings.get("festive")) {
+        $('#festive').html(`
+          <img src="/img/lights.gif" class='festive-image'>
+          `);
+      } else {
+        $('#festive').html(``);
+      }
     }
   }
 }

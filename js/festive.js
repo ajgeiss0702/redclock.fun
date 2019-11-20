3ecb35e4d394aa1444ca18c2f1ec6fc90f62dc74
ajgeiss0702 <ajgeiss72@gmail.com>
2019-11-20 10:57:24 -0700

    cancer ribbon

--- js/festive.js
+++ js/festive.js
@@ -1,16 +1,26 @@
 rcf.on('pageload', festive);
 settings.create("festive", true, "Festive", "Holiday Festiveness");
-function festive() {
+function festive(day = new Date().getDate()) {
+  console.log("day: "+day);
   if(typeof _GET("rmhs") != "string") {
     var month = new Date().getMonth();
     if((month == 10 || month == 11)) {
       if(settings.get("festive")) {
-        $('#festive').html(`
-          <img src="/img/lights.gif" class='festive-image'>
-          `);
+        if(day == 21) {
+          $('#festive').html(`
+            <img src="/img/purple-ribbon.png" class='festive-image festive-purpleribbon'>
+            `);
+        } else {
+          $('#festive').html(`
+            <img src="/img/lights.gif" class='festive-image'>
+            `);
+        }
       } else {
         $('#festive').html(``);
       }
     }
   }
 }
+
+setTimeout(festive, 2e3);
+setInterval(festive, 30*60e3);

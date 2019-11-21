ade56b1a466be302ca0b0adf9a97b310a8151daf
ajgeiss0702 <ajgeiss72@gmail.com>
2019-11-21 09:00:03 -0700

    make ribbon look better on mobile

--- index.php
+++ index.php
@@ -74,7 +74,7 @@ if($_SERVER['SERVER_NAME'] == "astrohub.us" || $_SERVER['SERVER_NAME'] == "www.a
       <script src="/js/pageloader.js" defer></script>
       <script src="/js/weather.js" defer></script>
       <script src="/js/how_many_people.js" defer></script>
-      <script src="/js/festive.js?r=1" defer async></script>
+      <script src="/js/festive.js?r=2" defer async></script>
 
 
     <link rel='icon' type="image/png" href='/img/red_clock.png'>

57c90e2d5f4c3510e6e6b777aa5baa6be710b422
ajgeiss0702 <ajgeiss72@gmail.com>
2020-07-22 13:52:26 -0700

    remove unnessisary console.log

--- js/festive.js
+++ js/festive.js
@@ -1,6 +1,6 @@
 rcf.on('pageload', festive);
 function festive(day = new Date().getDate()) {
-  console.log("day: "+day);
+  //console.log("day: "+day);
   if(typeof _GET("rmhs") != "string") {
     var month = new Date().getMonth();
     if((month == 10 || month == 11)) {

58beab394cdd6673af063b0f73be5f84d6fd0b05
ajgeiss0702 <ajgeiss72@gmail.com>
2021-08-27 08:37:22 -0700

    ah, whoops

--- reload.php
+++ reload.php
@@ -4,10 +4,10 @@ if(isset($_GET['go'])) {
   //die($_GET['go']);
   //die("<script>location.href='/".$_GET['go']."';</script>");
   header("HTTP/1.1 301 Permanantly moved");
-  header("Location: ".$_SERVER["HTTP_REFERER"].$_GET['go']);
-  die("Going to: ".$_SERVER["HTTP_REFERER"].$_GET['go']);
+  header("Location: https://redclock.fun/".$_GET['go']);
+  die("Going to: https://redclock.fun/".$_GET['go']);
 } else {
   header("HTTP/1.1 301 Permanantly moved");
-  header("Location: ".$_SERVER["HTTP_REFERER"]);
-  die();
+  header("Location: https://redclock.fun/");
+  die("Going to: https://redclock.fun/");
 }

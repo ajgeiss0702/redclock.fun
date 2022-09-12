0f555dd5f09ce33fc495e6f436d109ff9856dd7d
ajgeiss0702 <ajgeiss72@gmail.com>
2022-09-12 10:14:42 -0700

    catch error

--- src/routes/schools/+page.js
+++ src/routes/schools/+page.js
@@ -1,3 +1,11 @@
 export async function load({ fetch }) {
-    return await fetch("https://ajg0702.us/api/rmf/schedule.php?school=list").then((response) => response.json())
+    return await fetch("https://ajg0702.us/api/rmf/schedule.php?school=list")
+        .then((response) => response.json())
+        .catch(() => {
+            return {
+                "error": {
+                    "display": "Error"
+                }
+            }
+        })
 }

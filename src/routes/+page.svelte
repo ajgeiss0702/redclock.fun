4f3eee5c703bf2a5fac2080a53c95607c0e1575b
ajgeiss0702 <ajgeiss72@gmail.com>
2022-09-19 19:10:53 -0700

    Add loading svg to root (for slower connections to not have a blank page)

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -21,3 +21,20 @@
         }
     })
 </script>
+
+<style>
+    div {
+        position: fixed;
+        top: 0;
+        left: 0;
+
+        height: 100vh;
+        width: 100vw;
+        z-index: -100;
+        background-image: url("/img/loading.svg");
+        background-position: center;
+        background-repeat: no-repeat;
+    }
+
+</style>
+<div></div>

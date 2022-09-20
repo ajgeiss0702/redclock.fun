4d6c4a22bb9f2ac16952726ba8dc481d91f934ac
ajgeiss0702 <ajgeiss72@gmail.com>
2022-09-19 19:30:17 -0700

    about page

---
+++ src/routes/about/+page.svelte
@@ -0,0 +1,26 @@
+<br>
+<img src="/red_clock.png" style="min-width: 10vw; max-height: 25vh;">
+<br>
+<br>
+<br>
+<br>
+This website is made to count down until the bell rings at your school.
+You can select which school you are at using the <a href="/schools">school selector</a>.<br>
+<br>
+This website was started at Red Mountain High School, but is slowly expanding to more schools.<br>
+If you would like the site to expand to your school, fill out the form linked on the school selector.
+
+<br><br><br><br>
+<hr>
+<br>
+<h3>Credits</h3>
+<div>Weather disabled icon made by
+    <a href="https://www.flaticon.com/authors/smashicons" title="Smashicons">Smashicons</a>
+    from
+    <a href="https://www.flaticon.com/" title="Flaticon">
+        www.flaticon.com
+    </a> is licensed by
+    <a href="http://creativecommons.org/licenses/by/3.0/" title="Creative Commons BY 3.0" target="_blank">CC 3.0 BY</a><br>
+    <br>
+    <div>Copy quote icon made by <a href="https://www.flaticon.com/authors/google" title="Google">Google</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a></div>
+</div>

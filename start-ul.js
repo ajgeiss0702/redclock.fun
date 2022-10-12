69fda991cd438c95a2b6269354328e44aee9d1a0
ajgeiss0702 <ajgeiss72@gmail.com>
2022-10-12 12:14:18 -0700

    start-ul.js custom endpoints

--- start-ul.js
+++ start-ul.js
@@ -1,6 +1,29 @@
 import {exec} from 'child_process';
+import express from 'express';
 
-console.log("Installing required packages");
-exec("npm i && npm run build", () => {
-    import("./build-node/index.js");
+console.log("Building server");
+exec("npm run build", async () => {
+
+    const app = express();
+
+    // note to self: custom endpoints go HERE, not below
+
+    app.get('/healthcheck', (req, res) => {
+        res.end('ok');
+    });
+
+    const handler = (await import('./build-node/handler.js')).handler;
+    app.use(handler);
+
+
+    const server = app.listen(3000, () => {
+        console.log('Listening on port 3000');
+    });
+
+    process.on('SIGTERM', () => {
+        console.debug('SIGTERM signal received: closing HTTP server')
+        server.close(() => {
+            console.debug('HTTP server closed')
+        })
+    })
 })

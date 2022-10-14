1f817b1252d01782e90e577300b766b88d5c578d
ajgeiss0702 <ajgeiss72@gmail.com>
2022-10-14 10:49:25 -0700

    center about page

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -1,26 +1,35 @@
-<br>
-<img src="/red_clock.png" style="min-width: 10vw; max-height: 25vh;" alt="Red Clock logo">
-<br>
-<br>
-<br>
-<br>
-This website is made to count down until the bell rings at your school.
-You can select which school you are at using the <a href="/schools">school selector</a>.<br>
-<br>
-This website was started at Red Mountain High School, but is slowly expanding to more schools.<br>
-If you would like the site to expand to your school, fill out the form linked on the school selector.
-
-<br><br><br><br>
-<hr>
-<br>
-<h3>Credits</h3>
-<div>Weather disabled icon made by
-    <a href="https://www.flaticon.com/authors/smashicons" title="Smashicons">Smashicons</a>
-    from
-    <a href="https://www.flaticon.com/" title="Flaticon">
-        www.flaticon.com
-    </a> is licensed by
-    <a href="http://creativecommons.org/licenses/by/3.0/" title="Creative Commons BY 3.0" target="_blank">CC 3.0 BY</a><br>
+<style>
+    div {
+        text-align: center;
+    }
+</style>
+<div>
     <br>
-    <div>Copy quote icon made by <a href="https://www.flaticon.com/authors/google" title="Google">Google</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a></div>
+    <img src="/red_clock.png" style="min-width: 10vw; max-height: 25vh;" alt="Red Clock logo">
+    <br>
+    <br>
+    <br>
+    <br>
+    This website is made to count down until the bell rings at your school.
+    You can select which school you are at using the <a href="/schools">school selector</a>.<br>
+    <br>
+    This website was started at Red Mountain High School, but is slowly expanding to more schools.<br>
+    If you would like the site to expand to your school, fill out the form linked on the school selector.
+
+    <br><br><br><br>
+    <hr>
+    <br>
+    <h3>Credits</h3>
+    <div>
+        Weather disabled icon made by
+        <a href="https://www.flaticon.com/authors/smashicons" title="Smashicons">Smashicons</a>
+        from
+        <a href="https://www.flaticon.com/" title="Flaticon">
+            www.flaticon.com
+        </a> is licensed by
+        <a href="http://creativecommons.org/licenses/by/3.0/" title="Creative Commons BY 3.0" target="_blank">CC 3.0 BY</a><br>
+        <br>
+        <div>Copy quote icon made by <a href="https://www.flaticon.com/authors/google" title="Google">Google</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a></div>
+    </div>
+
 </div>

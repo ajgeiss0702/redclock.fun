e88f46e28ac1950a76da914a225de31c1c39e77c
ajgeiss0702 <ajgeiss72@gmail.com>
2022-10-25 11:41:18 -0700

    i test

--- src/routes/api/test/+server.ts
+++ src/routes/api/test/+server.ts
@@ -1,7 +1,9 @@
+let i = 0;
+
 export function GET() {
     let start = Date.now();
     let dateString = new Date().toString();
-    return new Response(String(dateString) + "\noog",
+    return new Response(String(dateString) + "\noog " + i++,
         {
             headers: {
                 "Server-Timing": "hello;desc=\"hello there, general kenobi\";dur=0, dateString;dur=" + (Date.now() - start)

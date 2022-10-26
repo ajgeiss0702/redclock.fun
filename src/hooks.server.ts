5bd33fbbd243baccb8eb549e84167b49b52c2a64
ajgeiss0702 <ajgeiss72@gmail.com>
2022-10-25 22:07:53 -0700

    allow origin on api route

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -3,7 +3,9 @@ import type {Handle} from "@sveltejs/kit";
 export const handle: Handle = async ({ event, resolve }) => {
     const response = await resolve(event);
 
-    response.headers.set("Access-Control-Allow-Origin", "*");
+    if(event.url.pathname.startsWith("/api")) {
+        response.headers.set("Access-Control-Allow-Origin", "*");
+    }
 
     return response;
 }

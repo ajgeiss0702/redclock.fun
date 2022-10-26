7b8d9cd0909e24f8148d380cc5925a8ba301c767
ajgeiss0702 <ajgeiss72@gmail.com>
2022-10-26 11:00:06 -0700

    cache bust

--- index.php
+++ index.php
@@ -186,7 +186,7 @@ if($_SERVER['SERVER_NAME'] == "astrohub.us" || $_SERVER['SERVER_NAME'] == "www.a
       <script src='/js/countdown-utils.js?c=11' defer async></script>
       <script src="/js/pageloader.js" defer></script>
       <script src="/js/weather.js?r=1" defer></script>
-      <script src="/js/how_many_people.js" defer></script>
+      <script src="/js/how_many_people.js?r=1" defer></script>
       <script src="/js/festive.js?r=5" defer async></script>
       <script src="/js/quotes.js?r=4" defer async></script>
       <script src="/js/clock.js?r=3" defer async></script>

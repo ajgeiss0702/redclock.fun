e7db8203c6c99f76ed0d13a18e0dafa21d3ddbd3
ajgeiss0702 <ajgeiss72@gmail.com>
2022-10-26 11:55:23 -0700

    settings page note

--- src/routes/settings/+page.svelte
+++ src/routes/settings/+page.svelte
@@ -1,6 +1,16 @@
 <script>
     import {Button} from "sveltestrap";
 </script>
-
-WIP
-<Button outline color="secondary" href="/countdown">Back</Button>
+<style>
+    div {
+        text-align: center;
+    }
+</style>
+<div>
+    <Button outline color="secondary" href="/countdown">Back</Button>
+    <br>
+    <h1>Settings page coming soon</h1>
+    Other than themes and layouts (and the settings that are in the settings tab), im not sure what settings I should have here.<br>
+    <br>
+    If you have ideas for more settings there should be, let me know using any of the contact methods mentioned in the news about this (in the news tab on the main page)
+</div>

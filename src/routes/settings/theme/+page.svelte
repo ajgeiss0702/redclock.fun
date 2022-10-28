3186e44fd629ee5f913876e11f063be43e0f05fc
ajgeiss0702 <ajgeiss72@gmail.com>
2022-10-27 20:31:06 -0700

    theme page style improvements

--- src/routes/settings/theme/+page.svelte
+++ src/routes/settings/theme/+page.svelte
@@ -41,40 +41,52 @@
         display: none !important;
     }
 
+    .table-container {
+        padding: 1em;
+    }
+
     .theme-select-box {
-        width: 12em;
+        width: 7em;
+    }
+
+    .title {
+        width: 10em;
     }
 
     .invisible-text {
         color: rgba(0, 0, 0, 0);
     }
 
-    @media (min-width: 1000px) {
+    @media (min-width: 500px) {
         table {
-            max-width: 1000px;
+            max-width: 500px;
         }
     }
 </style>
 <h1>Theme Settings</h1>
 
-<table>
-    <tr>
-        <td>Follow System Theme</td>
-        <td>
-            <label class="tgl tgl-gray">
-                <input type="checkbox" checked={initialFollowSystem}  on:change={toggleFollowSystem}>
-                <span data-on="On" data-off="Off"></span>
-            </label>
-        </td>
-    </tr>
-    <tr class:invisible-text={followSystem}>
-        <td>Theme</td>
-        <td class="theme-select-box">
-            <select class="form-select d-inline-block mw-100" class:hidden={followSystem} on:change={changeTheme} bind:this={themeSelectBox}>
-                <option selected={initialTheme === "light"} value="light">Light</option>
-                <option selected={initialTheme === "dark"} value="dark">Dark</option>
-                <option selected={initialTheme === "black"} value="black">Black</option>
-            </select>
-        </td>
-    </tr>
-</table>
+<div class="table-container">
+    <table>
+        <tr>
+            <td class="title">Follow System Theme</td>
+            <td class="spacer"></td>
+            <td>
+                <label class="tgl tgl-gray">
+                    <input type="checkbox" checked={initialFollowSystem}  on:change={toggleFollowSystem}>
+                    <span data-on="On" data-off="Off"></span>
+                </label>
+            </td>
+        </tr>
+        <tr class:invisible-text={followSystem}>
+            <td class="title">Theme</td>
+            <td class="spacer"></td>
+            <td class="theme-select-box">
+                <select class="form-select d-inline-block mw-100" class:hidden={followSystem} on:change={changeTheme} bind:this={themeSelectBox}>
+                    <option selected={initialTheme === "light"} value="light">Light</option>
+                    <option selected={initialTheme === "dark"} value="dark">Dark</option>
+                    <option selected={initialTheme === "black"} value="black">Black</option>
+                </select>
+            </td>
+        </tr>
+    </table>
+</div>

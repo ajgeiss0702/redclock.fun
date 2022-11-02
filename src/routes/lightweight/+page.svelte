f475680e0e9d879e5b863eb90a9a76158084356d
ajgeiss0702 <ajgeiss72@gmail.com>
2022-11-01 21:37:33 -0700

    lightweight explainer page

--- src/routes/lightweight/+page.svelte
+++ src/routes/lightweight/+page.svelte
@@ -1,5 +1,15 @@
 <script>
     import CountdownBox from "$lib/countdown/CountdownBox.svelte";
 </script>
-
+<style>
+    div {
+        text-align: center;
+    }
+</style>
 <CountdownBox box={false}/>
+<br>
+<br>
+<br>
+<div>
+    <a href="/lightweight/what" class="btn btn-outline-secondary">What is this?</a>
+</div>

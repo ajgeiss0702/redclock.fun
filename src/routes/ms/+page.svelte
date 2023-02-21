40656d6921e0776c1d25452fd303df960d59798b
ajgeiss0702 <ajgeiss72@gmail.com>
2023-02-20 21:32:25 -0700

    also give the cool thing its own page

---
+++ src/routes/ms/+page.svelte
@@ -0,0 +1,19 @@
+<script>
+    import Minesweeper from "$lib/countdown/sidebar/minesweeper/Minesweeper.svelte";
+</script>
+
+<style>
+    div {
+        height: 100vh;
+
+        margin-left: auto;
+        margin-right: auto;
+
+        display: flex;
+        justify-content: center;
+        align-items: center;
+    }
+</style>
+<div>
+    <Minesweeper/>
+</div>

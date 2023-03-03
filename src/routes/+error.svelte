6bbffbd764772fb24c2fb9cbe8faa24984a3c463
ajgeiss0702 <ajgeiss72@gmail.com>
2023-03-03 13:55:43 -0700

    slightly better error page

---
+++ src/routes/+error.svelte
@@ -0,0 +1,26 @@
+<script>
+    import { page } from '$app/stores';
+</script>
+<style>
+    div.outer {
+        display: flex;
+        height: 100vh;
+        align-items: center;
+        justify-content: center;
+    }
+    div.inner {
+        text-align: center;
+    }
+    img {
+        max-width: 90vw;
+    }
+</style>
+
+<div class="outer">
+    <div class="inner">
+        <img src="/img/broken-clock.png" alt="Broken Clock"><br>
+        <br>
+        <h1>{$page.status}</h1>
+        {$page.error.message}
+    </div>
+</div>

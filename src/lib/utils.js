b2395c3d7970c4b89a73b427e945fd08ca2b9899
ajgeiss0702 <ajgeiss72@gmail.com>
2023-03-12 13:27:00 -0700

    whoops

--- src/lib/utils.js
+++ src/lib/utils.js
@@ -1,4 +1,4 @@
-import {makeDate} from "$lib/countdown/countdown-utils.js";
+import {makeDate} from "$lib/countdown/countdown-utils.ts";
 import {create, get} from "$lib/settings";
 import {browser, dev, building as prerendering} from "$app/environment";
 

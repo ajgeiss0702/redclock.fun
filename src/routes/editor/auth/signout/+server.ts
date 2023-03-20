92faa99257cb4f13319e92f9b902dea8d6d9154a
ajgeiss0702 <ajgeiss72@gmail.com>
2023-03-19 18:03:53 -0700

    signout functionality

--- src/routes/editor/auth/signout/+server.ts
+++ src/routes/editor/auth/signout/+server.ts
@@ -1,6 +1,22 @@
 import type {RequestHandler} from "@sveltejs/kit";
-import {text} from "@sveltejs/kit";
+import {error, redirect, text} from "@sveltejs/kit";
+import {dev} from "$app/environment";
 
-export const GET = (() => {
-    return text("TODO");
+export const GET = (({platform, cookies}) => {
+    const sessionId = cookies.get("session");
+    if(!sessionId) throw redirect(302, "/editor");
+
+    if(!dev) {
+        if(!platform?.env?.D1DB) {
+            throw error(500, "no db!")
+        }
+
+        platform.env.D1DB.prepare("delete from sessions where id=?")
+            .bind(sessionId)
+            .run()
+    }
+
+    cookies.delete("session", {path: "/"});
+
+    throw redirect(302, "/editor");
 }) satisfies RequestHandler;

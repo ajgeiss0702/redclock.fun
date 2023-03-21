28caaddd831a962b9628443b01b81ad6413ac6a5
ajgeiss0702 <ajgeiss72@gmail.com>
2023-03-20 21:14:44 -0700

    (editor) fix weird flicker issue on hover

--- src/lib/editor/SchoolSelect.svelte
+++ src/lib/editor/SchoolSelect.svelte
@@ -1,17 +1,15 @@
 <script lang="ts">
     export let school: EditorSchool;
 </script>
-<a href="/editor/school/{school.code}" class="hidden-link">
-    <div class="school-container rounded-container-token border-token border-surface-200-700-token p-2 text-left mx-auto my-3 card-hover flex">
-        <div class="flex-none self-center">
-            <img src={school.logo} alt="{school.code}">
-        </div>
-        <div class="flex-1 self-center">
-            <h2>{school.display}</h2>
-            <span class="text-surface-400-500-token">
+<a href="/editor/school/{school.code}" class="hidden-link school-container rounded-container-token border-token border-surface-200-700-token p-2 text-left mx-auto my-3 card-hover flex">
+    <div class="flex-none self-center">
+        <img src={school.logo} alt="{school.code}">
+    </div>
+    <div class="flex-1 self-center">
+        <h2>{school.display}</h2>
+        <span class="text-surface-400-500-token">
                 {school.code}
             </span>
-        </div>
     </div>
 </a>
 

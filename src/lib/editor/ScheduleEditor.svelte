3fe3971ee8a7f409bc3b16338cfdc863db63674c
ajgeiss0702 <ajgeiss72@gmail.com>
2023-04-13 10:30:48 -0700

    editor progress

--- src/lib/editor/ScheduleEditor.svelte
+++ src/lib/editor/ScheduleEditor.svelte
@@ -7,7 +7,42 @@
     export let schedule: ScheduleTimes;
     export let schedules: {[key: string]: string} = {};
 
-    $: console.log({schedule})
+    let loopableSchedule = (() => {
+        let list = [];
+        for (let scheduleCode in schedule) {
+            let scheduleOrigData = schedule[scheduleCode];
+            let scheduleData = [];
+            for (const className in scheduleOrigData) {
+                let classTimes = scheduleOrigData[className];
+                scheduleData.push({
+                    name: className,
+                    times: classTimes
+                });
+            }
+            list.push({
+                code: scheduleCode,
+                schedule: scheduleData
+            });
+        }
+        return list;
+    })()
+
+    export let scheduleOut = {};
+    
+    $: scheduleOut = (() => {
+        let o = {};
+        for (const {code, schedule} of loopableSchedule) {
+            let times = {};
+            for (let c of schedule) {
+                times[c.name] = c.times;
+            }
+            o[code] = times;
+        }
+        console.log({scheduleOut: o})
+        return o;
+    })();
+
+    $: console.log({loopableSchedule})
 </script>
 <style>
     .schedulesContainer {
@@ -22,15 +57,15 @@
 
 <div class="schedulesContainer m-2 mx-auto">
     <Accordion>
-        {#each Object.entries(schedule) as [scheduleCode, schedule]}
-            <AccordionItem open>
-                <svelte:fragment slot="summary">{schedules[scheduleCode] || scheduleCode}</svelte:fragment>
+        {#each loopableSchedule as {code, schedule}}
+            <AccordionItem>
+                <svelte:fragment slot="summary">{schedules[code] ?? code}</svelte:fragment>
                 <svelte:fragment slot="content">
                     <table class="mx-auto">
-                        {#each Object.entries(schedule) as [entryName, time]}
+                        {#each schedule as {name, times}}
                             <tr>
-                                <td class="px-4"><input type="text" bind:value={entryName}></td>
-                                <td class="px-4">{dateString(makeDate(time))}</td>
+                                <td class="px-4"><input type="text" bind:value={name}></td>
+                                <td class="px-4">{dateString(makeDate(times))}</td>
                             </tr>
                         {/each}
                     </table>
@@ -41,3 +76,4 @@
         {/each}
     </Accordion>
 </div>
+<pre class="text-left">{JSON.stringify(scheduleOut, null, '\t')}</pre>

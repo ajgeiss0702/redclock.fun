e0eaaa3f48ce8c75c5af1281459cc5fac5511c6c
ajgeiss0702 <ajgeiss72@gmail.com>
2023-04-14 15:05:56 -0700

    Try a new one

--- src/routes/(info)/api/weather/get/[school]/+server.ts
+++ src/routes/(info)/api/weather/get/[school]/+server.ts
@@ -5,7 +5,7 @@ const cacheTime = 20 * 60 * 1000;
 
 
 const accounts = [
-    "698923a1672a28fdabe4c0d6a23a7fa3", // bill
+    "cdc3cd09a24d81122f1fcddef05f82b1", // bill
     "bf26c7abb25893e59cc5a0afeb62b36c", // ajgeiss72
     "f2cfe4c7ea4ff8ab12b151b45c9723a0" // aiden
 ];

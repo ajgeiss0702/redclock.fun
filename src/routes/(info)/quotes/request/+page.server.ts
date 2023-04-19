1ed8e7a1d5ccb3bdfaa39e7c2c2965ba30ca32ec
ajgeiss0702 <ajgeiss72@gmail.com>
2023-04-18 23:24:43 -0700

    woohoo more testing in prod because i cant reproduce it in dev

--- src/routes/(info)/quotes/request/+page.server.ts
+++ src/routes/(info)/quotes/request/+page.server.ts
@@ -5,6 +5,7 @@ import type {ServerLoad} from "@sveltejs/kit";
 let vpnIp = "149.28.87.60";
 
 export const load = (async ({getClientAddress}) => {
+    console.log("got " + getClientAddress())
     let admin = dev || getClientAddress() === vpnIp;
     return {admin}
 }) satisfies ServerLoad

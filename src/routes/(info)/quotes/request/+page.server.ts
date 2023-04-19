ea65d5452ee222eafd94ff5886d3e4c8d80301fc
ajgeiss0702 <ajgeiss72@gmail.com>
2023-04-18 23:39:10 -0700

    ipv6

--- src/routes/(info)/quotes/request/+page.server.ts
+++ src/routes/(info)/quotes/request/+page.server.ts
@@ -3,9 +3,10 @@ import type {ServerLoad} from "@sveltejs/kit";
 
 
 let vpnIp = "149.28.87.60";
+let vpnIpv6 = "2001:19f0:6001:1ca2:5400:03ff:feac:5f2e";
 
 export const load = (async ({getClientAddress}) => {
     console.log("got " + getClientAddress())
-    let admin = dev || getClientAddress() === vpnIp;
+    let admin = dev || getClientAddress() === vpnIp || getClientAddress() === vpnIpv6;
     return {admin}
 }) satisfies ServerLoad

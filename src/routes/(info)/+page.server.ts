6169733c461c8324c13001d297fed6beddc3a586
ajgeiss0702 <ajgeiss72@gmail.com>
2023-04-19 21:47:57 -0700

    whoops

--- src/routes/(info)/+page.server.ts
+++ src/routes/(info)/+page.server.ts
@@ -3,8 +3,14 @@ import {redirect} from "@sveltejs/kit";
 
 
 export const load = (async ({cookies, url}) => {
-    if(!cookies.get("school") && !cookies.get("schedule")) return {};
-    if(url.searchParams.has("noRedirect")) return {};
+
+    const school = cookies.get("school");
+    const schedule = cookies.get("schedule");
+
+    if(!school && !schedule) return {}
+    if(url.searchParams.has("noRedirect")) return {school, schedule};
 
     if(cookies.get("alwaysRedirect") === "true") throw redirect(302, "/countdown");
+
+    return {school, schedule};
 }) satisfies ServerLoad;

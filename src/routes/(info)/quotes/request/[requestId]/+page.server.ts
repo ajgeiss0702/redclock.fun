c2413df0b0f93811a7d78bc96911225f83056574
ajgeiss0702 <ajgeiss72@gmail.com>
2023-04-22 14:11:11 -0700

    whoops

--- src/routes/(info)/quotes/request/[requestId]/+page.server.ts
+++ src/routes/(info)/quotes/request/[requestId]/+page.server.ts
@@ -2,7 +2,7 @@ import {dev} from "$app/environment";
 import type {ServerLoad} from "@sveltejs/kit";
 import {error} from "@sveltejs/kit";
 
-export const load = (async ({platform, locals, params}) => {
+export const load = (async ({platform, params}) => {
     const id = params.requestId;
     if(!id) throw error(400, "Need a request ID");
     if(dev) return {
@@ -22,7 +22,9 @@ export const load = (async ({platform, locals, params}) => {
     let kv = platform?.env?.QUOTE_SUGGESTIONS;
     if(!kv) return {};
 
-    const {value, metadata} = await kv.getWithMetadata(id);
+    const {value, metadata} = await kv.getWithMetadata(id, {type: "json"});
+
+    if(!value) throw error(404, "Quote not found");
 
 
     return {
---
+++ src/routes/(info)/quotes/request/[requestId]/+page.svelte
@@ -0,0 +1,10 @@
+<script>
+
+    export let data;
+</script>
+
+Quote: {data?.value?.quote}<br>
+Author: {data?.value?.author}<br>
+Note: {data?.value?.author}<br>
+Status: {data?.metadata?.status}<br>
+Submitted: {new Date(data?.metadata?.submitted)}

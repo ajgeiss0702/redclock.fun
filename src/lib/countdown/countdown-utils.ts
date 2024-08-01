b37d0bdcf80a8f8c6d8b8638db1f3c8b6f967e19
ajgeiss0702 <aj@ajg0702.us>
2024-08-01 09:48:54 -0500

    move hosting of schedules somewhere else for now

--- src/lib/countdown/countdown-utils.ts
+++ src/lib/countdown/countdown-utils.ts
@@ -56,7 +56,7 @@ export function getSchedule(school: string, schedule: string): Promise<SchoolDat
 
     scheduleCache[key] = {
         lastGet: Date.now(),
-        lastResp: fetch('https://ajg0702.us/api/rmf/schedule.php?school='+school)
+        lastResp: fetch('https://temp-schedules.redclock.fun/schedule.php?school='+school)
             .then(r => r.json())
             .then(j => j[school])
     };
@@ -246,7 +246,7 @@ export function schoolExists(key: string) {
     if(Object.keys(existsCache).includes(key) && Date.now() - existsExpiry[key] < 30 * 60e3) { // cache for 30 minutes
         return existsCache[key];
     }
-    let promise = fetch('https://ajg0702.us/api/rmf/schedule.php?exists='+key)
+    let promise = fetch('https://temp-schedules.redclock.fun/schedule.php?exists='+key)
         .then(r => r.json())
         .then(j => (j.exists as boolean));
 
@@ -261,7 +261,7 @@ export async function scheduleExists(school: string, schedule: string) {
     if(Object.keys(existsCache).includes(school+":"+schedule) && Date.now() - existsExpiry[school+":"+schedule] < 30 * 60e3) { // cache for 30 minutes
         return existsCache[school+":"+schedule];
     }
-    let promise = fetch('https://ajg0702.us/api/rmf/schedule.php?school='+school)
+    let promise = fetch('https://temp-schedules.redclock.fun/schedule.php?school='+school)
         .then(r => r.json())
         .then(j => !!j[school]?.schedules?.[schedule]);
 
